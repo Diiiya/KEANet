3d387f749311923adeac61fd4ce3f619bf3a670f
Diiiya <33103806+Diiiya@users.noreply.github.com>
2020-03-30 21:41:48 +0300

    Initial commit

---
+++ src/main/java/Purchase.java
@@ -0,0 +1,22 @@
+
+public class Purchase {
+	
+	private Boolean _internetConnection;
+	private int _phoneLines;
+	
+	public Boolean get_internetConnection() {
+		return _internetConnection;
+	}
+	public void set_internetConnection(Boolean _internetConnection) {
+		this._internetConnection = _internetConnection;
+	}
+	public int get_phoneLines() {
+		return _phoneLines;
+	}
+	public void set_phoneLines(int _phoneLines) {
+		this._phoneLines = _phoneLines;
+	}
+	
+	
+
+}
