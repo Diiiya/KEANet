d7c0771368a7a81cc015694123b69a95e3cbf2d6
moonlaughs <iza8848@gmail.com>
2020-03-31 10:13:34 +0200

    Purchase methods and one unit test

--- src/main/java/Purchase.java
+++ src/main/java/Purchase.java
@@ -1,10 +1,12 @@
+import java.util.List;
+import java.util.ArrayList;
 
 public class Purchase {
 	
 	private Boolean _internetConnection;	
 	private int _phoneLines;
 	private String[] _cellPhones;
-	private int _price;
+	private int _price;	
 	
 	public Boolean get_internetConnection() {
 		return _internetConnection;
@@ -31,6 +33,45 @@ public class Purchase {
 		this._price = _price;
 	}
 	
+
+	//phones that client chose
+	List<String> _myPhones = new ArrayList();
+	public List<String> get_myPhones() {
+		return _myPhones;
+	}
+	public void set_myPhones(List<String> _myPhones) {
+		this._myPhones = _myPhones;
+	}
+	
+	//Catalog of the phones
+	List<Phone> _phoneCatalog = new ArrayList();
+	private void set_phoneCatalog(List<Phone> _phoneCatalog) {		
+		Phone p1;
+		Phone p2;
+		Phone p3;
+		Phone p4;
+		Phone p5;
+		
+		p1 = new Phone("Motorola G99", 800);
+		p2 = new Phone("iPhone 99", 6000);
+		p3 = new Phone("Samsung Galaxy 99", 1000);
+		p4 = new Phone("Sony Xperia 99", 900);
+		p5 = new Phone("Huawei 99", 900);
+
+		_phoneCatalog.add(p1);
+		_phoneCatalog.add(p2);
+		_phoneCatalog.add(p3);
+		_phoneCatalog.add(p4);
+		_phoneCatalog.add(p5);
+		
+		this._phoneCatalog = _phoneCatalog;
+	}
+	
+	private List<Phone> get_phoneCatalog(){
+		return _phoneCatalog;
+	}
+	
+	
 	public Purchase(Boolean _internetConnection, int _phoneLines, String[] _cellPhones, int _price) {
 		super();
 		this._internetConnection = _internetConnection;
@@ -39,4 +80,56 @@ public class Purchase {
 		this._price = _price;
 	}
 	
+	
+	public int DecrementingTheNumberOfPhoneLines() {
+		if(get_phoneLines() != 0) {
+			set_phoneLines(_phoneLines - 1);
+			set_price(_price - 150);
+		}
+		return get_price();
+			
+	}
+	
+	public int UnselectingACellPhone(String _modelName) {
+		if(get_myPhones().size() != 0) {
+			for (int i=0; i<get_myPhones().size(); i++) 
+			{ 
+			    if(get_myPhones().get(i) == _modelName) {
+			    	get_myPhones().remove(i); //remove from array
+			    	for(Phone item : get_phoneCatalog()) {
+			    		//get Price from the catalog
+			    		if(item.get_name() == _modelName) {
+					    	//decrease price
+			    			set_price(get_price()- item.get_price());
+			    		}
+			    	}
+			    }
+			}
+		}
+		return get_price();
+	}
+	
+	public String Buying() {
+		if((get_internetConnection() == false || get_internetConnection() == null) && get_phoneLines() == 0 &&  get_myPhones().size() == 0 && get_price() == 0) {
+			return "Nothing Selected";
+		}
+		else {
+			String myAlert = "You chose: ";
+			
+			if(get_internetConnection() == true) {
+				myAlert += "Internet Connection, ";
+			}
+			if(get_phoneLines() > 0) {
+				myAlert += get_phoneLines() + "7 Phone Lines,";
+			}
+			if(get_myPhones().size() > 0) {
+				myAlert += "Phones that you chose: ";
+				for(String item : get_myPhones()) {
+					myAlert += item + ", ";
+				}
+			}
+			myAlert += "Total Price: " + get_price();
+			return myAlert;
+		}
+	}
 }
--- src/test/java/PurchaseTest.java
+++ src/test/java/PurchaseTest.java
@@ -8,5 +8,37 @@ public class PurchaseTest {
 	public void test() {
 		fail("Not yet implemented");
 	}
+	
+	//Test cases pass: 1,4,7,8     => Total price should be decreased by 150
+	//test cases fail: -10,-1,0,9,15  => Total price not changed
+	@Test
+	public void testDecrementingTheNumberOfPhoneLines_returnsDecrementedPrice() {
+		//Arrange
+		String[] _cellPhones = new String[2];
+		_cellPhones[0] = "iPhone 99";
+		_cellPhones[1] = "Motorola G99";
+		
+		Purchase object = new Purchase(true, 1, _cellPhones, 7150); //fake object
+		int[] testPhoneLines= {-10,-1,0,1,4,7,8,9,15}; //test cases
+		
+		for(int item : testPhoneLines) {
+			object.set_phoneLines(item);
+			int totalPrice = object.DecrementingTheNumberOfPhoneLines();
+			
+			if(item > 0 && item < 9) {
+				assertEquals(7000, totalPrice);
+			}
+			else {
+				assertEquals(7150, totalPrice);
+			}
+		}
+	}
+	
+	@Test
+	public void testUnselectingACellPhone_returnDecreasedPrice() {
+		//Arrange
+		//Act
+		//Assert
+	}
 
 }
